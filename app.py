53cbdeb3ae00f0b7db181f6484c998a6925ede09
Rohit Sharma <rohit@debian-BULLSEYE-live-builder-AMD64>
2022-01-11 12:41:36 +0530

    flask-form init

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def func():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def form_submit():
+    if request.method == 'POST':
+        return render_template('land.html', empid = request.form['empid'], empname = request.form['empname'], qual = request.form['qual'], desg = request.form['desg'], sal = request.form['salary'])
+
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+#{{ url_for('static', filename='css/style.css') }}
